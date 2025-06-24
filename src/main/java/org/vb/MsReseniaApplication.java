e8a5cfd3b9cdf5c04fbd9c1123950c94d799431e
Hayleen <haivcarr@espol.edu.ec>
2025-06-24 11:30:03 -0500

    Base del proyecto y dependencias

---
+++ src/main/java/org/vb/MsReseniaApplication.java
@@ -0,0 +1,13 @@
+package org.vb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+@EnableJpaAuditing
+public class MsReseniaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MsReseniaApplication.class, args);
+    }
+}
